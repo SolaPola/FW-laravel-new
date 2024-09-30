c30e5c801f60277846364b4a689f69e5558b290e
Sola Ogino <solapolacola@gmail.com>
2024-09-30 14:22:13 +0200

    episode 7

---
+++ resources/views/home.blade.php
@@ -0,0 +1,12 @@
+<x-layout>
+    
+<x-slot:heading>
+    Home Page
+</x-slot:heading> 
+
+
+
+
+
+
+</x-layout> 
---
+++ routes/web.php
@@ -0,0 +1,75 @@
+<?php
+
+use Illuminate\Support\Arr;
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+// Route::get('/', function () {
+//     return view('home', [
+//         'greeting' => 'Hiiii!!!!!'
+//     ]);
+// });
+
+Route::get('/', function () {
+    return view('home', []);
+});
+
+Route::get('/jobs', function () {
+    return view('jobs', [
+        'jobs' => [
+            [
+                'id' => 1,
+                'title' => 'Director',
+                'salary' => '$50,000'
+            ],
+            [
+                'id' => 2,
+                'title' => 'Programmer',
+                'salary' => '$10,000'
+            ],
+            [
+                'id' => 3,
+                'title' => 'Teacher',
+                'salary' => '$40,000'
+            ],
+        ]
+    ]);
+});
+
+Route::get('/jobs/{id}', function ($id) {
+
+    $jobs = [
+        [
+            'id' => 1,
+            'title' => 'director',
+            'salary' => '$50,000'
+
+        ],
+        [
+            'id' => 2,
+            'title' => 'programmer',
+            'salary' => '$10,000'
+        ],
+        [
+            'id' => 3,
+            'title' => 'teacher',
+            'salary' => '50,000$'
+        ],
+    ];
+   $job = Arr::first($jobs, fn($job) => $job['id'] == $id);
+    dd($job);
+   return view('job', ['job' => $job]);
+});
+
+Route::get('/contact', function () {
+    return view('contact');
+});
