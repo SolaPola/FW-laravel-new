41227da799485188b7e588042656c63f31caf294
Sola Ogino <solapolacola@gmail.com>
2024-10-07 14:08:50 +0200

    ep8

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,9 @@
 <?php
 
-use Illuminate\Support\Arr;
 use Illuminate\Support\Facades\Route;
+use App\Models\Job;
+
+
 
 
 Route::get('/', function () {
@@ -10,49 +12,14 @@ Route::get('/', function () {
 
 Route::get('/jobs', function () {
     return view('jobs', [
-        'jobs' => [
-            [
-                'id' => 1,
-                'title' => 'Director',
-                'salary' => '$50,000'
-            ],
-            [
-                'id' => 2,
-                'title' => 'Programmer',
-                'salary' => '$10,000'
-            ],
-            [
-                'id' => 3,
-                'title' => 'Teacher',
-                'salary' => '$40,000'
-            ],
-        ]
+        'jobs' => Job::all()
     ]);
 });
 
 Route::get('/jobs/{id}', function ($id) {
+    $job = Job::find($id);
 
-    $jobs = [
-        [
-            'id' => 1,
-            'title' => 'director',
-            'salary' => '$50,000'
-
-        ],
-        [
-            'id' => 2,
-            'title' => 'programmer',
-            'salary' => '$10,000'
-        ],
-        [
-            'id' => 3,
-            'title' => 'teacher',
-            'salary' => '50,000$'
-        ],
-    ];
-   $job = Arr::first($jobs, fn($job) => $job['id'] == $id);
-    dd($job);
-   return view('job', ['job' => $job]);
+    return view('job', ['job' => $job]);
 });
 
 Route::get('/contact', function () {
