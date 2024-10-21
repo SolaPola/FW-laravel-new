c6bab85c6d045e1b1bffd1c2129e76b37b7fd349
Sola Ogino <solapolacola@gmail.com>
2024-10-21 09:47:05 +0200

    ep12

--- app/Models/Job.php
+++ app/Models/Job.php
@@ -5,16 +5,10 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 
-// use Illuminate\Support\Arr;
-
-
-class Job extends Model
-{
-
+class Job extends Model {
     use HasFactory;
 
     protected $table = 'job_listings';
-
 
     protected $fillable = ['title', 'salary'];
 
@@ -23,15 +17,9 @@ class Job extends Model
         return $this->belongsTo(Employer::class);
     }
 
+    public function tags()
+    {
+        return $this->belongsToMany(Tag::class, foreignPivotKey: "job_listing_id");
 
-    // public static function find(int $id): array
-    // {
-    //     $job = Arr::first(static::all(), fn($job) => $job['id'] == $id);
-
-    //     if (! $job) {
-    //         abort(404); 
-
-    //     }
-    //     return $job;
-    // }
+    }
 }
