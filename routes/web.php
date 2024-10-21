d847c7b3e06bd81ea4307e48b65cb64474b537cd
Sola Ogino <solapolacola@gmail.com>
2024-10-21 16:22:03 +0200

    ep14

--- routes/web.php
+++ routes/web.php
@@ -3,18 +3,15 @@
 use Illuminate\Support\Facades\Route;
 use App\Models\Job;
 
-
 Route::get('/', function () {
-
-
-    return view('home', []);
+    return view('home');
 });
 
-
-
 Route::get('/jobs', function () {
+    $jobs = Job::with('employer')->simplePaginate(3);
+
     return view('jobs', [
-        'jobs' => Job::all()
+        'jobs' => $jobs
     ]);
 });
 
